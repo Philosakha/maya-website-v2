4bb6a251a86ec6201bf4c4a45138f13fa286ebd2
kaushikchoudhury <kaushikc44@gmail.com>
2024-01-21 22:27:44 +0530

    added the team component

---
+++ src/components/team.tsx
@@ -0,0 +1,33 @@
+
+
+export default function TeamComponent(){
+    return (
+        <>
+
+<div className="justify-center items-center ">
+                <h1 className="text-2xl justify-center items-center">Meet the Team</h1>
+    
+                <div className="flex flex-row justify-center items-center">
+                    <div className="card w-96 bg-primary text-primary-content">
+                    <div className="card-body">
+                        <h2 className="card-title">Kaushik Choudhury </h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                        <button className="btn">Buy Now</button>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="card w-96 bg-primary text-primary-content">
+                    <div className="card-body">
+                        <h2 className="card-title">Harsh Ghodkar</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                        <button className="btn">Buy Now</button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+</div>
+        </>
+    )
+}
