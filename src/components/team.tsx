6a36dee9037556e509f333b54e7b17a910f5ca5e
kaushikchoudhury <kaushikc44@gmail.com>
2024-01-23 19:14:06 +0530

    added images adn react icons

--- src/components/team.tsx
+++ src/components/team.tsx
@@ -1,4 +1,7 @@
 import Image from "next/image"
+import { CiLinkedin } from "react-icons/ci";
+import { CiTwitter } from "react-icons/ci";
+import { FaGithub } from "react-icons/fa";
 
 
 export default function TeamComponent(){
@@ -14,22 +17,29 @@ export default function TeamComponent(){
                    
                     <div className="card w-96 bg-secondary text-primary-content">
                     <div className="card-body">
-                    <div className=" justify-center w-full items-center">
+                    <div className=" justify-center w-full items-center flex flex-row space-x-5">
                         <Image src="/images/kaushik.jpg" alt="Kaushik choudhury" width={100} height={100} className="rounded-full justify-center items-center"/>
+                        <p className="text-2xl ">FullStack developer</p>
                     </div>
                         <h2 className="card-title text-white">Kaushik Choudhury </h2>
-                        <div className="card-actions justify-end">
-                        <button className="btn">Buy Now</button>
+                        <div className="card-actions justify-start space-x-5">
+                        <CiLinkedin size={50}/>
+                        <CiTwitter size={50}/>
+                        <FaGithub size={50}/>
                         </div>
                     </div>
                     </div>
                     <div className="card w-96 bg-primary text-primary-content">
                     <div className="card-body">
-                    <Image src="" alt="Kaushik choudhury" className="justify-center" />
+                    <div className=" justify-center w-full items-center flex flex-row space-x-5">
+                        <Image src="/images/kaushik.jpg" alt="Kaushik choudhury" width={100} height={100} className="rounded-full justify-center items-center"/>
+                        <p className="text-2xl ">BlockChain developer</p>
+                    </div>
                         <h2 className="card-title">Harsh Ghodkar</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions justify-end">
-                        <button className="btn">Buy Now</button>
+                        <div className="card-actions justify-start space-x-5">
+                        <CiLinkedin size={50}/>
+                        <CiTwitter size={50}/>
+                        <FaGithub size={50}/>
                         </div>
                     </div>
                     </div>
